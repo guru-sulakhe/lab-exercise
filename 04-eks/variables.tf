7b2501df3aa05962d592873ef0dd903ee026d033
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-06-27 19:31:36 +0530

    creating variables for eks cluster

--- 04-eks/variables.tf
+++ 04-eks/variables.tf
@@ -1,4 +1,16 @@
 variable "cluster_name" {
+    default = "lab-k8"
+}
+variable "project_name" {
     default = "lab-exercise"
 }
-variable ""
+variable "environment" {
+    default = "dev"
+}
+variable "common_tags" {
+    default = {
+        Project = "lab-exercise"
+        Environment = "dev"
+        Terraform = true
+    }
+}
