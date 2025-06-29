7a986584730d128d31c1c9b97ae2e8bfa16cfe6c
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-06-29 12:17:19 +0530

    Creating aws ec2 instances-jenkins,jenkins-agent

---
+++ 05-cicd-tools/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "5.64.0"
+            region = "us-east-1"
+        }
+    }
+    backend "s3" {
+        bucket = "guru97s-remote-state"
+        key = "lab-exercise-infra-cicd"
+        region = "us-east-1"
+        dynamodb_table = "guru97s-locking-dynamodb"
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
---
+++ 05-cicd-tools/variables.tf
@@ -0,0 +1,12 @@
+variable "project_name" {
+    default = "lab-exercise"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "zone_name" {
+    type = string
+    default = "guru97s.cloud"
+}
